48a3a6bc41f6a1715505575701f30232593ef979
Marco <marco.reisinger@t-online.de>
2023-09-06 22:12:44 +0200

    added basic landing page

---
+++ src/layouts/RasterLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr fFf">
+    <q-page-container class="row q-py-md">
+      <div class="col-xs-12 col-sm-12 col-md-3"></div>
+      <div class="col-xs-12 col-sm-12 col-md-6">
+        <router-view @tourAction="$emit('tourAction', $event)" />
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-3"></div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RasterLayout',
+});
+</script>
