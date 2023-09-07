45d483339ae26a04b315e6f86d134136f4f43f3d
Marco <marco.reisinger@t-online.de>
2023-09-07 17:57:03 +0200

    improved landing

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Roaddata </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
