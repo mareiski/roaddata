0c761ba6b08575b2c039c53786d62932a4727407
Marco <marco.reisinger@t-online.de>
2023-09-09 12:46:56 +0200

    improvements

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
-        <q-toolbar-title> Roaddata </q-toolbar-title>
+        <q-toolbar-title> Roaddata - under construction </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
@@ -20,6 +20,45 @@
       <!-- ***** Preloader End ***** -->
 
       <router-view />
+
+      <footer style="min-height: 750px" class="column justify-between">
+        <!-- ***** Contact Form Start ***** -->
+        <div class="contact-form q-ml-lg q-mb-lg" style="max-width: 600px">
+          <div class="row q-gutter-md justify-center">
+            <div class="col-md-6 col-sm-12">
+              <q-input label="Full name" outlined="" :model-value="name">
+                <template v-slot:prepend>
+                  <q-icon name="person" />
+                </template>
+              </q-input>
+            </div>
+            <div class="col-md-6 col-sm-12 q-mb-lg">
+              <q-input label="E-Mail Adress" outlined :model-value="email">
+                <template v-slot:prepend>
+                  <q-icon name="person" />
+                </template>
+              </q-input>
+            </div>
+            <div class="col-lg-12 q-mt-lg">
+              <q-btn color="primary" rounded size="lg"
+                >Request free trial</q-btn
+              >
+            </div>
+          </div>
+        </div>
+        <!-- ***** Contact Form End ***** -->
+
+        <!-- ***** Footer Start ***** -->
+        <div class="container q-px-md">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="sub-footer">
+                <p>Copyright &copy; 2023 Roadtrip Insights | Roaddata</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </q-page-container>
   </q-layout>
 </template>
